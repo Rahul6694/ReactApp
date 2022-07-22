09d2009e683d89d73761fb1eebc2735997ec6764
Rahul6694 <87510576+Rahul6694@users.noreply.github.com>
2022-07-22 23:27:03 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71453a606e72d8da41fc448e701289fa.js"
+  "/Rahul6694/ReactApp.git/precache-manifest.ac0db9f225db8ee0a6c117868f58d3ff.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Rahul6694/ReactApp.git/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
